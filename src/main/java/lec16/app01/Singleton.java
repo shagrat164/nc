24fdcf7d13b806c0f9e3d2a140e17082237629ba
shagrat <dprotsvetov@gmail.com>
2016-11-20 21:23:22 +0300

    commit

--- src/main/java/lec16/app01/Singleton.java
+++ src/main/java/lec16/app01/Singleton.java
@@ -1,16 +1,21 @@
 package lec16.app01;
 
 
-public class Singleton {
-    private static Singleton ourInstance;
+class Singleton {
+    private static final int MAXIMUM = 5;
+    private int id;
+    private static int count;
+//    private static Singleton ourInstance;
 
-    public static Singleton getInstance() {
-        if (ourInstance == null) {
-             ourInstance = new Singleton();
-        }
-        return ourInstance;
+    static synchronized Singleton getInstance() {
+
+//        if (ourInstance == null) {
+//             ourInstance = new Singleton();
+//        }
+//        return ourInstance;
     }
 
     private Singleton() {
+        this.id++;
     }
 }
