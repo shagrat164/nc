9400349f24f8efd422d7984d444595b30e272d3d
shagrat <dprotsvetov@gmail.com>
2016-11-26 21:46:30 +0300

    битва роботов

--- src/lec19/Arena.java
+++ src/lec19/Arena.java
@@ -1,24 +1,47 @@
 package lec19;
 
 public class Arena {
+    private Thread threadPlayer1;
+    private Thread threadPlayer2;
+    private Robot player1;
+    private Robot player2;
+
     public static void main(String[] args) throws InterruptedException {
-        final Arena arena = new Arena();
-        Robot player1 = new Robot("Иван", arena);
-        Robot player2 = new Robot("Василий", arena);
+        Arena arena = new Arena(new Robot("Иван"), new Robot("Василий"));
+        arena.startFight();
+        arena.waitWinner();
+        System.out.println("Игра окончена.");
+    }
 
-        player1.setEnemy(player2);
-        player2.setEnemy(player1);
+    public Arena(Robot player1, Robot player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.player1.setEnemy(player2);
+        this.player2.setEnemy(player1);
 
-        Thread threadPlayer1 = new Thread(player1);
-        Thread threadPlayer2 = new Thread(player2);
+        this.threadPlayer1 = new Thread(this.player1);
+        this.threadPlayer2 = new Thread(this.player2);
+    }
 
-        threadPlayer1.start();
-        threadPlayer2.start();
-
-//        arena.waitWinner();
+    private void startFight() {
+        this.threadPlayer1.start();
+        this.threadPlayer2.start();
     }
 
     private void waitWinner() throws InterruptedException {
+        this.threadPlayer1.join();
+        this.threadPlayer2.join();
 
+        System.out.println(getWinner());
+    }
+
+    private String getWinner() {
+        if (this.player1.getHealth() > 0) {
+            return this.player1.getName() + " победил. Осталось " + this.player1.getHealth() + "hp";
+        }
+        if (this.player2.getHealth() > 0) {
+            return this.player2.getName() + " победил. Осталось " + this.player2.getHealth() + "hp";
+        }
+        return null;
     }
 }
--- src/lec19/Robot.java
+++ src/lec19/Robot.java
@@ -7,24 +7,22 @@ public class Robot implements Runnable {
     private String name;
     private Robot enemy;
     private Random random;
-    private final Arena arena;
 
-    public Robot(String name, Arena arena) {
+    Robot(String name) {
         this.name = name;
         random = new Random();
-        health = 1000;
-        this.arena = arena;
+        health = 50;
     }
 
-    public void attack() {
+    private void attack() {
         int random = this.random.nextInt(1000);
         if (random == 5) {
-            enemy.setHealth(enemy.getHealth() - 5);
-            System.out.println("Робот " + name + " атаковал робота " + enemy.getName() + " и отнял 5hp жизни.");
+            enemy.health = enemy.health - 5;
+            System.out.println("Робот " + name + " атаковал робота " + enemy.name + " и отнял 5hp жизни.");
         }
     }
 
-    public void setEnemy(Robot enemy) {
+    void setEnemy(Robot enemy) {
         this.enemy = enemy;
     }
 
@@ -36,13 +34,9 @@ public class Robot implements Runnable {
         return health;
     }
 
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
     @Override
     public void run() {
-        while (this.health > 0 && enemy.getHealth() > 0) {
+        while (this.health > 0 && enemy.health > 0) {
             attack();
         }
     }
